5ffbf4daac17d38840733eaafac458b619bce5e3
Roland-Sankara <roland1sankara@gmail.com>
2022-07-08 02:40:02 +0300

    fetaure: create school and Course - create forms for creating new school and course

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './components/Dashboard';
 import Schools from './components/Schools';
 import Courses from './components/Courses/index';
 import SchoolForm from './components/Forms/SchoolForm';
+import CourseForm from './components/Forms/CourseForm';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {ContextProvider} from './context/AppContext';
 import {QueryClient, QueryClientProvider} from 'react-query';
@@ -28,6 +29,7 @@ function App() {
                 <Route path='/courses' element={<Courses/>}/>
                 <Route path='/contacts' element={<h1>Contacts Section</h1>}/>
                 <Route path='/schools/create' element={<SchoolForm/>}/>
+                <Route path='/courses/create' element={<CourseForm/>}/>
               </Routes>
             </MainAppWrapper>
           </Router>
